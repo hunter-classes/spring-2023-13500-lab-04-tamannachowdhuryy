735d01dd986bcb205af15765df8793503c536728
Tamanna Chowdhury <tamanna.chowdhury610@gmail.com>
2023-02-21 22:51:58 -0500

    UPDATED

--- main.cpp
+++ main.cpp
@@ -14,88 +14,88 @@ This will print out all the shapes that are stated
 
 int main(){
 //TASK A: BOX
-  std::cout << "Width: 3 \n";
-  std::cout << "Height: 5 \n";
-  std::cout << "Shape: \n";
-  std::cout << box(3, 5);
-  std::cout << "\n------------------\n";
+  std::cerr << "Width: 3 \n";
+  std::cerr << "Height: 5 \n";
+  std::cerr << "Shape: \n";
+  std::cerr << box(3, 5);
+  std::cerr << "\n------------------\n";
   
-  std::cout << "Width: 7 \n";
-  std::cout << "Height: 4 \n";
-  std::cout << "Shape: \n";
-  std::cout << box(7, 4);
-  std::cout << "\n------------------\n";
+  std::cerr << "Width: 7 \n";
+  std::cerr << "Height: 4 \n";
+  std::cerr << "Shape: \n";
+  std::cerr << box(7, 4);
+  std::cerr << "\n------------------\n";
   
 //TASK B: CHECKERBOARD
-  std::cout << "Width: 11 \n";
-  std::cout << "Height: 6 \n";
-  std::cout << "Shape: \n";
-  std::cout << checkerboard(11, 6);
-  std::cout << "\n------------------\n";
+  std::cerr << "Width: 11 \n";
+  std::cerr << "Height: 6 \n";
+  std::cerr << "Shape: \n";
+  std::cerr << checkerboard(11, 6);
+  std::cerr << "\n------------------\n";
 
-  std::cout << "Width: 13 \n";
-  std::cout << "Height: 7 \n";
-  std::cout << "Shape: \n";
-  std::cout << checkerboard(13, 7);
-  std::cout << "\n------------------\n";
+  std::cerr << "Width: 13 \n";
+  std::cerr << "Height: 7 \n";
+  std::cerr << "Shape: \n";
+  std::cerr << checkerboard(13, 7);
+  std::cerr << "\n------------------\n";
   
 //TASK C: CROSS
-  std::cout << "Size: 8 \n";
-  std::cout << "Shape: \n";
-  std::cout << cross(8);
-  std::cout << "\n------------------\n";
+  std::cerr << "Size: 8 \n";
+  std::cerr << "Shape: \n";
+  std::cerr << cross(8);
+  std::cerr << "\n------------------\n";
 
-  std::cout << "Size: 10 \n";
-  std::cout << "Shape: \n";
-  std::cout << cross(10);
-  std::cout << "\n------------------\n";
+  std::cerr << "Size: 10 \n";
+  std::cerr << "Shape: \n";
+  std::cerr << cross(10);
+  std::cerr << "\n------------------\n";
 
 //TASK D: LOWER
-  std::cout << "Size: 6 \n";
-  std::cout << "Shape: \n";
-  std::cout << lower(6);
-  std::cout << "\n------------------\n";
+  std::cerr << "Size: 6 \n";
+  std::cerr << "Shape: \n";
+  std::cerr << lower(6);
+  std::cerr << "\n------------------\n";
 
-  std::cout << "Size: 10 \n";
-  std::cout << "Shape: \n";
-  std::cout << lower(15);
-  std::cout << "\n------------------\n";
+  std::cerr << "Size: 10 \n";
+  std::cerr << "Shape: \n";
+  std::cerr << lower(15);
+  std::cerr << "\n------------------\n";
 
 //TASK E: UPPER
-  std::cout << "Size: 5 \n";
-  std::cout << "Shape: \n";
-  std::cout << upper(5);
-  std::cout << "\n------------------\n";
+  std::cerr << "Size: 5 \n";
+  std::cerr << "Shape: \n";
+  std::cerr << upper(5);
+  std::cerr << "\n------------------\n";
 
-  std::cout << "Size: 8 \n";
-  std::cout << "Shape: \n";
-  std::cout << upper(8);
-  std::cout << "\n------------------\n";
+  std::cerr << "Size: 8 \n";
+  std::cerr << "Shape: \n";
+  std::cerr << upper(8);
+  std::cerr << "\n------------------\n";
   
 //TASK F: UPSIDE-DOWN TRAPEZOID
-  std::cout << "Width: 12 \n";
-  std::cout << "Height: 5 \n";
-  std::cout << "Shape: \n";
-  std::cout << trapezoid(12, 5);
-  std::cout << "\n------------------\n";
+  std::cerr << "Width: 12 \n";
+  std::cerr << "Height: 5 \n";
+  std::cerr << "Shape: \n";
+  std::cerr << trapezoid(12, 5);
+  std::cerr << "\n------------------\n";
 
-  std::cout << "Width: 12 \n";
-  std::cout << "Height: 7 \n";
-  std::cout << "Shape: \n";
-  std::cout << trapezoid(12, 7);
-  std::cout << "\n------------------\n";
+  std::cerr << "Width: 12 \n";
+  std::cerr << "Height: 7 \n";
+  std::cerr << "Shape: \n";
+  std::cerr << trapezoid(12, 7);
+  std::cerr << "\n------------------\n";
   
 // TASK G: CHECKERBOARD (3X3)
-  std::cout << "Width: 16 \n";
-  std::cout << "Height: 11 \n"; 
-  std::cout << "Shape: \n";
-  std::cout << checkerboard3x3(16, 11);
-  std::cout << "\n------------------\n";
+  std::cerr << "Width: 16 \n";
+  std::cerr << "Height: 11 \n"; 
+  std::cerr << "Shape: \n";
+  std::cerr << checkerboard3x3(16, 11);
+  std::cerr << "\n------------------\n";
   
-  std::cout << "Width: 27 \n";
-  std::cout << "Height: 27 \n"; 
-  std::cout << "Shape: \n";
-  std::cout << checkerboard3x3(27, 27);
-  std::cout << "\n------------------\n";
+  std::cerr << "Width: 27 \n";
+  std::cerr << "Height: 27 \n"; 
+  std::cerr << "Shape: \n";
+  std::cerr << checkerboard3x3(27, 27);
+  std::cerr << "\n------------------\n";
 return 0; 
 }
